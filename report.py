5312edd42d71e0814de780a9fa9b86402071ba39
Venkatesh Duraiarasan <dev_venkat@outlook.com>
2017-02-15 11:39:26 +0530

    fixed report alignment

--- report.py
+++ report.py
@@ -39,18 +39,18 @@ if __name__ == '__main__':
     rows = c.fetchall()
     count = len(rows)
     ext, req, san, MNOVM, MNSPA, SVR_NO_C_GR = 0,0,0,0,0,0
-    print('{}	{}	{}			{}	{}	{}	{}	{}		{}'.format('area','unit','name','ext','req','san','MNOVM','MNSPA','SVR_NO_C_GR'))
-    print('{}	{}	{}			{}	{}	{}	{}	{}		{}'.format('====','====','====','===','===','===','=====','=====','==========='))
+    print('{}	{}	{}	{}	{}	{}	{}	{}		{}'.format('area','unit',('name'+(' '*20))[0:20],'ext','req','san','MNOVM','MNSPA','SVR_NO_C_GR'))
+    print('{}	{}	{}	{}	{}	{}	{}	{}		{}'.format('====','====',('===='+(' '*20))[0:20],'===','===','===','=====','=====','==========='))
     for x in rows:
         #print (x.keys())
         #['ac', 'unit', 'name', 'working_unit', 'ext', 'req', 'san', 'MNOVM', 'MNSPA', 'SVR_NO_C_GR']
-        print('{}	{}	{}			{}	{}	{}	{}	{}		{}'.format(x['ac'],x['unit'],x['name'],x['ext'],x['req'],x['san'],x['MNOVM'],x['MNSPA'],x['SVR_NO_C_GR']))
+        print('{}	{}	{}	{}	{}	{}	{}	{}		{}'.format(x['ac'],x['unit'],(x['name']+(' '*20))[0:20],x['ext'],x['req'],x['san'],x['MNOVM'],x['MNSPA'],x['SVR_NO_C_GR']))
         ext = ext + (x['ext'] or 0)
         req = req + (x['req'] or 0)
         san = san + (x['san'] or 0)
         MNOVM = MNOVM + (x['MNOVM'] or 0)
         MNSPA = MNSPA + (x['MNSPA'] or 0)
         SVR_NO_C_GR = SVR_NO_C_GR + (x['SVR_NO_C_GR'] or 0)
-    print('{}	{}	{}			{}	{}	{}	{}	{}		{}'.format('----','----','total',ext,req,san,MNOVM,MNSPA,SVR_NO_C_GR))
+    print('{}	{}	{}	{}	{}	{}	{}	{}		{}'.format('----','----',('total'+(' '*20))[0:20],ext,req,san,MNOVM,MNSPA,SVR_NO_C_GR))
     print('{0} records found'.format(count))
  
