5b953075733e1e793a0dbd38e7869e5c689e46df
Venkatesh Duraiarasan <dev_venkat@outlook.com>
2017-03-06 12:27:14 +0530

    multiple unit in a file will be detected while inserting

--- insert/sanction.py
+++ insert/sanction.py
@@ -1,22 +1,13 @@
-# script.py
-import argparse
-import pyexcel as pe
-import configparser 
-import os
-import sys
-import sqlite3
-import my_connection as connection
 
-#from insert import sanction as insert_sanc
 
-DB_URL = None
-global conn
-conn = None
-sheet = None
+# DB_URL = None
+# global conn
+# conn = None
+# sheet = None
 
-desg_ls=None
-unit_ls=None
-sect_ls=None
+# desg_ls=None
+# unit_ls=None
+# sect_ls=None
 
 def load_tables():
 	# conn.row_factory = sqlite3.Row
@@ -37,15 +28,10 @@ def load_tables():
 	global eis_ls
 	eis_ls = [ x[0] for x in c.fetchall()]
 
-eis_list = []
-working_unit = []
-not_first_row = False
+# eis_list = []
 
-def validate_row(row, ignore_multi_unit):
-	# hold all error list
+def validate_row(row):
 	err = []
-
-	# check wrong codes
 	if row['DSCD'] not in desg_ls:
 		err.append(" dscd("+row['DSCD']+")")
 	if row['SECTION_CD'] not in sect_ls:
@@ -55,38 +41,25 @@ def validate_row(row, ignore_multi_unit):
 	if row['ONROLL_UNIT'] not in unit_ls:
 		err.append(" roll_unit("+row['ONROLL_UNIT']+")")
 
-	# check duplicate eis in file
 	global eis_list
+
 	if str(row['EIS']) in eis_list:
 		err.append(" eis_repeat("+str(row['EIS'])+")")
 	else:
 		eis_list.append(str(row['EIS']))
 
-	# check duplicate eis in db
 	try:
 		if int(row['EIS']) in eis_ls:
 			err.append(" eis_dup_db("+str(row['EIS'])+")")
 	except ValueError as e:
 		err.append(" eis_err("+str(row['EIS'])+")")
-
-	# check if multiple working_unit present in file
-	global working_unit
-	global not_first_row
-	if not ignore_multi_unit:
-		if not_first_row:
-			if str(row['WORKING UNIT']) not in working_unit:
-				err.append(" multiple_work_unit("+str(row['WORKING UNIT'])+")")
-		else:
-			working_unit.append(str(row['WORKING UNIT']))
-			not_first_row = True
-	
-	# return err list
+		
 	if not err:
 		return None
 	else:
 		return err
 
-def read_file(xls_path, sheet_name, upload, ignore_multi_unit):
+def read_file(xls_path, sheet_name, upload):
 	# book = pe.get_book(file_name=os.path.normpath(xls_path))
 	# sheets = book.to_dict()
 	# for name in sheets.keys():
@@ -111,11 +84,11 @@ def read_file(xls_path, sheet_name, upload, ignore_multi_unit):
 	error_ls = []
 
 	for idx, record in enumerate(records):
-		err_row = validate_row(record, ignore_multi_unit)
+		err_row = validate_row(record)
 
 		if err_row:
 			error_ls.append(err_row)
-			print('ERR @ ROW {} => {}'.format(idx+2,validate_row(record, ignore_multi_unit)))
+			print('ERR @ ROW {} => {}'.format(idx+2,validate_row(record)))
 	if error_ls:
 		print('correct the above error and upload')
 	else:
@@ -131,31 +104,3 @@ def read_file(xls_path, sheet_name, upload, ignore_multi_unit):
 			conn.commit()
 			print('--->{0} records inserted sucessfully'.format(len(ls)))
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    # parser.add_argument(
-    #     'filename', metavar='int', type=int, choices=range(10),
-    #      nargs='+', help='give a file name')
-    parser.add_argument('filename',  help='give file name')
-    parser.add_argument('table',  help='e for inserting employee; s for inserting sanction')
-    parser.add_argument("-sh",'--sheetname',  help='give sheet name; type * to include all sheets')
-    parser.add_argument("-u", "--upload", action="store_true", help="to update/commit changes into database")
-    parser.add_argument("-im", "--ignore_multi_unit", action="store_true", help="to upload file with multiple units and suppress its errors")#default=max,
-
-    args = parser.parse_args()
-    print(args)
-    #read_config()
-    conn = connection.get_connection()
-
-    if args.table == 'e':
-        load_tables()
-        read_file(args.filename, args.sheetname, args.upload, args.ignore_multi_unit)
-    elif args.table == 's':
-        insert_sanc.load_tables()
-        #insert_sanc.read_file(args.filename, args.sheetname, args.upload)
-    else:
-    	print('supplied argument  or order of argument is wrong')
-    	sys.exit()
-
-  
